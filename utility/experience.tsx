35b35ed460ec329fae7c62773097e78c22b5c6bc
Ujjval Priyadarshi <ujjval.priyadarshi@krazybee.com>
2024-01-08 16:51:20 +0530

    Adding New In Experience

--- utility/experience.tsx
+++ utility/experience.tsx
@@ -1,37 +1,44 @@
 const experience = [
   {
-    companyName: 'Pristyn Care',
-    duration: "Nov'22 - Present",
-    techstack: ['Reactjs', 'Nextjs', 'SEO', 'Redux'],
+    companyName: "KreditBee",
+    duration: "Jan'24 - Present",
+    techstack: ["Javascript", "Python", "Django"],
+    summary: "Just Started",
+    points: [],
+  },
+  {
+    companyName: "Pristyn Care",
+    duration: "Nov'22 - Dec'23",
+    techstack: ["Reactjs", "Nextjs", "SEO", "Redux"],
     summary:
-      '<p>Collaborated in migrating the frontend service from Angular to Nextjs to enhance the performance, optimizing <span>Web-Vitals</span>, and improving <span>SEO</span> scores, elevating the overall online visibility and ranking of webpages with the help of <span>server-side rendering</span> of <span>Nextjs</span>. Also, provided a seamless experience for around <span>16 million</span> daily users.</p>',
+      "<p>Collaborated in migrating the frontend service from Angular to Nextjs to enhance the performance, optimizing <span>Web-Vitals</span>, and improving <span>SEO</span> scores, elevating the overall online visibility and ranking of webpages with the help of <span>server-side rendering</span> of <span>Nextjs</span>. Also, provided a seamless experience for around <span>16 million</span> daily users.</p>",
     points: [
-      '<div>Developed main profile pages for Doctors, Clinics, and Hospitals that can proficiently be managed and streamlined the entire book appointment and call doctor process</div>',
-      '<div>Developed listing pages named SRP pages for multiple doctors, clinics and Hospitals which can be filtered as per user preference.</div>',
-      '<div>Collaborated in building the Internal CRM Dashboard that uses <span>React</span> and <span>Redux</span> architecture which replaces the dependency of external organisation, Salesforce</div>',
+      "<div>Developed main profile pages for Doctors, Clinics, and Hospitals that can proficiently be managed and streamlined the entire book appointment and call doctor process</div>",
+      "<div>Developed listing pages named SRP pages for multiple doctors, clinics and Hospitals which can be filtered as per user preference.</div>",
+      "<div>Collaborated in building the Internal CRM Dashboard that uses <span>React</span> and <span>Redux</span> architecture which replaces the dependency of external organisation, Salesforce</div>",
     ],
   },
   {
-    companyName: 'Pasarpolis',
+    companyName: "Pasarpolis",
     duration: "June'21 - Nov'22",
-    techstack: ['Reactjs', 'Django', 'Redux', 'Python', 'Gatsby', 'MySql'],
+    techstack: ["Reactjs", "Django", "Redux", "Python", "Gatsby", "MySql"],
     summary:
-      '<p>Build and Maintained the internal dashboard, and multiple micro-services from both frontend and backend side.</p>',
+      "<p>Build and Maintained the internal dashboard, and multiple micro-services from both frontend and backend side.</p>",
     points: [
-      '<div>Developed UI of different standalone products for multiple partners such as Xiaomi, Shopee, VFX, etc.</div>',
-      '<div>Collaborated in automation for creating a whole policy creation flow that automates more than <span>5k products</span> for multiple partners..</div>',
-      '<div>Developed an application named <span>Merchantpanel</span>, from scratch using <span>Reactjs (Gatsby)</span> and <span>Django</span> which allows merchants to keep track of their commission and profits.</div>',
+      "<div>Developed UI of different standalone products for multiple partners such as Xiaomi, Shopee, VFX, etc.</div>",
+      "<div>Collaborated in automation for creating a whole policy creation flow that automates more than <span>5k products</span> for multiple partners..</div>",
+      "<div>Developed an application named <span>Merchantpanel</span>, from scratch using <span>Reactjs (Gatsby)</span> and <span>Django</span> which allows merchants to keep track of their commission and profits.</div>",
     ],
   },
   {
-    companyName: 'Soochi Techonology (Internship)',
+    companyName: "Soochi Techonology (Internship)",
     duration: "June'21 - Nov'22",
-    techstack: ['Python', 'Selenium', 'Machine Learning'],
+    techstack: ["Python", "Selenium", "Machine Learning"],
     points: [
-      '<div>Web-Scraping using <span>Selenium</span> and <span>Beautifulsoup</span></div>',
-      '<div>Build an Image Based Product Recommendation System that is used for Identifying products that visually look Similar using <span>k-nearest neighbor</span></div>',
+      "<div>Web-Scraping using <span>Selenium</span> and <span>Beautifulsoup</span></div>",
+      "<div>Build an Image Based Product Recommendation System that is used for Identifying products that visually look Similar using <span>k-nearest neighbor</span></div>",
     ],
   },
-]
+];
 
-export default experience
+export default experience;
